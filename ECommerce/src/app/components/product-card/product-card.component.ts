f2b6cf5353c54bce08292f5d21c7f608341cbc2d
Hieu <Admin@Kem>
2024-08-12 16:51:57 +0700

    list products

--- ECommerce/src/app/components/product-card/product-card.component.ts
+++ ECommerce/src/app/components/product-card/product-card.component.ts
@@ -1,23 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Product } from '../../../types/types';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
-
+import { Subject } from 'rxjs';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { Overlay } from 'primeng/overlay';
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule, CardModule, ButtonModule, RatingModule,FormsModule],
+  imports: [CommonModule, CardModule, ButtonModule, RatingModule, FormsModule, OverlayPanelModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
   @Input() product!: Product;
+  @ViewChild('op') overlay: Overlay | undefined;
+  showOverlay: boolean = false;
   ngOnInit() { }
 
   addProductToCart() {
 
   }
+
 }
